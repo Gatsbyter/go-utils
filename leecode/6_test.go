c4f38b6ecda3c21a272ba5f6eca4946310eeab09
rayholu <gatsbyter@gmail.com>
2021-02-25 21:46:58 +0800

    leecode

--- leecode/6_test.go
+++ leecode/6_test.go
@@ -1,4 +1,5 @@
 //给定一个仅包含 0 和 1 、大小为 rows x cols 的二维二进制矩阵，找出只包含 1 的最大矩形，并返回其面积。
+//https://leetcode-cn.com/problems/validate-binary-search-tree/
 
 package leecode
 
@@ -6,17 +7,67 @@ import (
 	"testing"
 )
 
-var matrix = [][]byte{
-	{1, 0, 1, 0, 0},
-	{1, 0, 1, 1, 1},
-	{1, 1, 1, 1, 1},
-	{1, 0, 0, 1, 0},
-}
-
 func Test_6(t *testing.T) {
-	t.Log(maximalRectangle(matrix))
+	t.Log(isValidBST(&Node{}))
 }
 
-func maximalRectangle(matrix [][]byte) int {
-	return 0
+type Node struct {
+	Val   int
+	Left  *Node
+	Right *Node
+}
+
+func isValidBST(root *Node) bool {
+	if root == nil {
+		return true
+	}
+
+	ok, _, _ := is(root)
+
+	return ok
+}
+
+func is(root *Node) (bool, int, int) {
+	var (
+		tmpMax int
+		tmpMin int
+	)
+
+	if root.Left != nil {
+		ok, max, min := is(root.Left)
+		if !ok || max >= root.Val {
+			return false, 0, 0
+		}
+
+		tmpMin = Min(min, root.Val)
+	} else {
+		tmpMin = root.Val
+	}
+
+	if root.Right != nil {
+		ok, max, min := is(root.Right)
+		if !ok || min <= root.Val {
+			return false, 0, 0
+		}
+
+		tmpMax = Max(max, root.Val)
+	} else {
+		tmpMax = root.Val
+	}
+
+	return true, tmpMax, tmpMin
+}
+
+func Max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func Min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
 }
